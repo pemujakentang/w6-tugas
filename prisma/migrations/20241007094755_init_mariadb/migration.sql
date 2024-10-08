b79cb6783d737e0334cbc0e3a7e8bcc4fee495cb
Rivo Juicer Wowor <rivowowor7@gmail.com>
2024-10-08 15:38:28 +0700

    fix: broken migrations

--- prisma/migrations/20241007094755_init_mariadb/migration.sql
+++ prisma/migrations/20241007094755_init_mariadb/migration.sql
@@ -6,6 +6,5 @@ CREATE TABLE `GuestbookEntry` (
     `comment` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `GuestbookEntry_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
